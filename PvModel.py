bac213de38f65e5c5d4ae359f4d2bc9a5284e485
Dirk van den Biggelaar <31848372+dirkbig@users.noreply.github.com>
2017-11-15 10:54:35 +0100

    Revert "ABM_trading_algorithm_dev"

--- PvModel.py
+++
@@ -1,9 +0,0 @@
-class PvModel:
-    def __init__(self, eta, S):
-        self.eta = eta
-        self.S = S
-
-    def get_output(self, irradiance):
-        output = self.eta * self.S * irradiance
-        return (output)
-
--- results.py
+++
@@ -1,9 +0,0 @@
-from run_real import *
-import matplotlib.pyplot as plt
-
-
-
-
-plt.plot(load_file)
-plt.plot(agent_file)
-plt.show()
